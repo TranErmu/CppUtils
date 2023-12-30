d8eb5ca2127369a957460d4eb866cfef7de46a70
tranlin <tranlin_d@163.com>
2023-12-30 19:13:08 +0800

    optimize

--- cmake-template/cmake/sub_template.cmake
+++ cmake-template/cmake/sub_template.cmake
@@ -1,5 +1,4 @@
 # set target name
-# default is the name of the current folder
 get_filename_component(dir_name ${CMAKE_CURRENT_LIST_DIR} NAME)
 set (PRESENT_TARGET ${dir_name})
 
@@ -12,13 +11,15 @@ foreach(file ${SRCS})
         message(STATUS ${file})
 endforeach()
 
-# target_link_libraries()
 
 add_executable(${PRESENT_TARGET} ${SRCS})
+
+# target_link_libraries()
+# target_include_directories(${PROJ_NAME} PUBLIC include)
+
 install(TARGETS ${PRESENT_TARGET}
         LIBRARY DESTINATION ${PRESENT_TARGET}                   # 动态库安装路径
         ARCHIVE DESTINATION ${PRESENT_TARGET}                   # 静态库安装路径
         RUNTIME DESTINATION ${PRESENT_TARGET}                   # 可执行文件安装路径
         PUBLIC_HEADER DESTINATION ${PRESENT_TARGET}/include/    # 头文件安装路径
         )
-# target_include_directories(${PROJ_NAME} PUBLIC include)
